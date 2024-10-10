4264858043b120bff9e1ba7b6f52c62b86e688da
megha371 <meghareddy371@gmail.com>
2024-10-10 13:58:04 +0530

    appscrip

--- src/App.js
+++ src/App.js
@@ -11,71 +11,66 @@ function App() {
   }, []);
  
   return (
-    <div className="App">
-      
-   <h1>LOGO</h1>
-   <div>
-    <h1>Shop   Skills   Stories   AboutUS</h1>
-    
-   </div>
-     <h1>Discover our products</h1>
-   
-<div className='container'>
-<div className="filter-section">
-  
-        <label htmlFor="category-select" type="checkbox">customizable</label>
-        <p>ideal</p>
-        <select>
-          <option value="all">All</option>
+    <div className='App'>
+      <h1>LOGO</h1>
+      <div>
+        <h1>Shop Skills Stories AboutUS</h1>
+        <h5>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et leo
+          pellentesque, scelerisque orci at, <br/>dapibus purus. Morbi viverra auctor
+          erat at tincidunt. Curabitur vel dolor libero
+        </h5>
+      </div>
+      <h1>Discover our products</h1>
+
+      <div className='container'>
+        <div className='filter-section'>
+          <label htmlFor='category-select' type='checkbox'>
+            customizable
+          </label>
+          <p>ideal</p>
+          <select>
+            <option value='all'>All</option>
           </select>
           <p>occasion</p>
-          <select 
-         
-        >
-          <option value="all" id="all">all</option>
+          <select>
+            <option value='all' id='all'>
+              all
+            </option>
           </select>
           <p>work</p>
 
-          <select 
-        
-        >
-          <option value="all">all</option>
+          <select>
+            <option value='all'>all</option>
           </select>
           <p>fabric</p>
 
-          <select
-          
-        >
-          <option value="all">all</option>
+          <select>
+            <option value='all'>all</option>
           </select>
           <p>segment</p>
 
-          <select >
-          <option value="all">all</option>
-        </select>
-        <p>suitable</p>
-        <select>
-          <option value="all">All</option>
+          <select>
+            <option value='all'>all</option>
+          </select>
+          <p>suitable</p>
+          <select>
+            <option value='all'>All</option>
           </select>
           <p>Rawmaterial</p>
-        <select>
-          <option value="all">All</option>
+          <select>
+            <option value='all'>All</option>
           </select>
+        </div>
+        <div className='productgrid'>
+          {products.map((product) => (
+            <div key={product.id} className='productcard'>
+              <img src={product.image} alt={product.title} />
+              <h2>{product.title} </h2>
+            </div>
+          ))}
+        </div>
       </div>
-     <div className='productgrid'>
-{
-  products.map(product=>(
-
-    <div key={product.id} className='productcard'>
-
-      <img src={product.image} alt={product.title}/>
-      <h2>{product.title}   </h2>
-    </div>
-  ))
-}
-</div>
-
-     </div>
     </div>
   );
 }
